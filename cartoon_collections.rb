dcbf87708026bcde9739177dedd09115a741ea3f
Christopher Leja <christopher.leja@gmail.com>
2020-02-10 23:03:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarf_names)
 
   roll_call = dwarf_names.each_with_index do|dwarf, index| puts "#{index + 1}. #{dwarf}"
 end
-puts roll_call
+#puts roll_call
 end
 
 def summon_captain_planet(array)
